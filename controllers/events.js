f941b89afaa6920334bb5c5af159c94cbc3cf953
Abraham <abrahamlega@gmail.com>
2023-11-20 21:14:54 +0000

    Lets gooo

---
+++ controllers/events.js
@@ -0,0 +1,127 @@
+const { respose } = require("express");
+const Evento = require("../models/evento");
+
+const getEventos = async (req, res = response) => {
+  try {
+    const eventos = await Evento.find().populate("user", "name");
+
+    return res.status(200).json({
+      ok: true,
+      eventos,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({
+      ok: false,
+      msg: "Error al recuperar los eventos",
+    });
+  }
+};
+
+const createEvento = async (req, res = response) => {
+  const evento = new Evento(req.body);
+  try {
+    evento.user = req.uid;
+    const eventoDB = await evento.save();
+
+    return res.status(200).json({
+      ok: true,
+      evento: eventoDB,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      msg: "Error. Contacte con el administrador.",
+    });
+  }
+};
+
+const updateEvento = async (req, res = response) => {
+  console.log("update evento");
+  const { id } = req.params;
+  const { uid } = req;
+
+  try {
+    const evento = await Evento.findById(id);
+    console.log(evento);
+
+    if (!evento) {
+      return res.status(404).json({
+        ok: false,
+        msg: "El evento no existe",
+      });
+    }
+
+    if (evento.user.toString() !== uid) {
+      return res.status(401).json({
+        ok: false,
+        msg: "No tiene privilegios de edicion",
+      });
+    }
+
+    const nuevoEvento = {
+      ...req.body,
+      user: uid,
+    };
+
+    const eventoActualizado = await Evento.findByIdAndUpdate(id, nuevoEvento, {
+      new: true,
+    });
+
+    return res.status(200).json({
+      ok: true,
+      evento: eventoActualizado,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      msg: "Hable con el administrador",
+    });
+  }
+};
+
+const deleteEvento = async (req, res = response) => {
+  const { id } = req.params;
+  const { uid } = req;
+
+  try {
+    const evento = await Evento.findById(id);
+    if (!evento) {
+      return res.status(404).json({
+        ok: false,
+        msg: "El evento no existe",
+      });
+    }
+
+    if (evento.user.toString() !== uid) {
+      return res.status(401).json({
+        ok: false,
+        msg: "No tiene privilegios de eliminación",
+      });
+    }
+
+    const { deletedCount } = await Evento.deleteOne(evento);
+
+    if (deletedCount > 0) {
+      return res.status(200).json({
+        ok: true,
+        removed: deletedCount,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      msg: "Hable con el administrador",
+    });
+  }
+};
+
+module.exports = {
+  getEventos,
+  createEvento,
+  updateEvento,
+  deleteEvento,
+};
---
+++ routes/events.js
@@ -0,0 +1,35 @@
+/* 
+    Rutas de eventos / Events
+    host + /api/events
+*/
+
+const { Router } = require("express");
+const { check } = require("express-validator");
+const {
+  getEventos,
+  createEvento,
+  updateEvento,
+  deleteEvento,
+} = require("../controllers/events");
+const { validarJwt } = require("../middlewares/validar-jwt");
+const { validarCampos } = require("../middlewares/validar-campos");
+const { isDate } = require("../helpers/isDate");
+
+const router = Router();
+router.use(validarJwt);
+
+router.get("/", getEventos);
+
+router.post("/", [
+  check('title', 'El título es obligatorio').not().isEmpty(),
+  check('start', 'Fecha de inicio es obligatoria').isDate(),
+  //check('start', 'Fecha de inicio es obligatoria').custom(isDate),
+  check('end', 'Fecha de finalización es obligatoria').isDate(),
+  validarCampos
+],
+createEvento);
+router.put("/:id", updateEvento);
+
+router.delete("/:id", deleteEvento);
+
+module.exports = router;
